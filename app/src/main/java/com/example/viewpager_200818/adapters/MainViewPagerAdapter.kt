4513f7e2035073f352727aed6a5ede091d74c73d
박새 <yannn7979@gmail.com>
2020-08-18 21:20:28 +0900

    각 페이지별 제목 설정

--- app/src/main/java/com/example/viewpager_200818/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpager_200818/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,21 @@ import com.example.viewpager_200818.fragments.NameFragment
 import com.example.viewpager_200818.fragments.PhoneNumFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        //charsequence는 string이랑 비슷한거구나 생각하면 됨
+        if (position==0){
+            return "NAME"
+        }
+        else if (position==1){
+            return "PHONE"
+        }
+        else {
+            return "Addr"
+        }
+    }
+
+
     override fun getItem(position: Int): Fragment {
 
         if(position == 0){
